cfc28c99e2b92a15d70641f04635ff3c975fccdf
stephdinu <stephaniedinu@gmail.com>
2023-07-13 11:03:51 +0200

    spring mvc + clean build

--- src/test/java/com/example/accessingdatamysql/AccessingDataMysqlApplicationTests.java
+++ src/test/java/com/example/accessingdatamysql/AccessingDataMysqlApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.accessingdatamysql;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AccessingDataMysqlApplicationTests.class)
 class AccessingDataMysqlApplicationTests {
 
 	@Test
